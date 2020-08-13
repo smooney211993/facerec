38d4423c71148f3d4d89db9c308b542782b27d3a
smooney211993 <sm.athome21@gmail.com>
2020-08-13 12:40:59 +1000

    added disclaimer

--- src/Components/Rank/Rank.js
+++ src/Components/Rank/Rank.js
@@ -5,12 +5,17 @@ const Rank = (props) =>{
     const {imageCount, user} = props;
     return (
         <div>
-            <div className='white f3'>
-                {user.name}, your current face count is'
+            <div className='white f3 pa2'>
+                {user.name}, your current face count is
+                
             </div>
             <div className="white f3">
                 {imageCount}
 
+            </div>
+            <br></br>
+            <div className ='f7'>
+                    Please make sure of decent image quality in order for the face detection to work!
             </div>
         </div>
     )
